e69247e15734133b311c2803e4f93df47a36df0d
tonyibra <tonyibra.ads@gmail.com>
2021-07-12 21:02:23 +0300

    ADD : temp units

--- src/Redux/Services/openWeatherAPI.js
+++ src/Redux/Services/openWeatherAPI.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const openWeatherAPI = {};
 
-openWeatherAPI.getCountrybyCountryName = async (searchString) => {
-  const endpoint = `api.openweathermap.org/data/2.5/weather?q=${searchString}&appid=fe59f54d9745f8b87580dcc5b70f2ed0`;
+openWeatherAPI.getCountrybyCountryName = async (
+  searchString,
+  unit = "metric"
+) => {
+  const endpoint = `//api.openweathermap.org/data/2.5/weather?q=${searchString}&units=${unit}&appid=fe59f54d9745f8b87580dcc5b70f2ed0`;
   try {
     const response = await axios.get(`${endpoint}`, {});
     if (response.data) {
