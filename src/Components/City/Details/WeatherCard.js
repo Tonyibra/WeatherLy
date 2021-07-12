f132d44a2a921361d43529204b06be64a8c7db16
tonyibra <tonyibra.ads@gmail.com>
2021-07-13 00:04:24 +0300

    add: weather decription

--- src/Components/City/Details/WeatherCard.js
+++ src/Components/City/Details/WeatherCard.js
@@ -5,6 +5,7 @@ const WeatherCard = ({ cityData }) => {
   console.log(cityData);
   const date = moment().format("LT");
   const getTemp = Math.floor(cityData.main?.temp);
+  const getDescription = cityData.weather[0]?.description;
   const Celsius = new Intl.NumberFormat("en", {
     style: "unit",
     unit: "celsius",
@@ -19,6 +20,9 @@ const WeatherCard = ({ cityData }) => {
       </div>
       <div className={styles.temp}>
         <span>{Celsius.format(getTemp)}</span>
+      </div>
+      <div className={styles.descriptiom}>
+        <span>{getDescription}</span>
       </div>
     </div>
   );
