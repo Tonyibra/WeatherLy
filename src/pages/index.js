7cdcf0d96320f7e908d649495d14fcfb59dbcecb
tonyibra <tonyibra.ads@gmail.com>
2021-07-13 19:32:38 +0300

    bug fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import React from "react";
 import LandingPage from "../Components/Landing/LandingPage";
 
 export default function Home() {
+  React.useEffect(() => {
+    localStorage.setItem("temp", "Celcius");
+  }, []);
+
   return (
     <div>
       <Head>
